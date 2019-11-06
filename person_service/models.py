e536d8dab9a9976f19adab5b0e40ff639075fb61
Adam Przywuski <adamprzywuski98@gmail.com>
2019-11-06 21:36:53 +0100

    Adding generating date which includes statisc born in Poland in years 1970-2014

--- person_service/models.py
+++ person_service/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.utils import timezone
 import random
+from datetime import datetime
+from random import randrange
+from datetime import timedelta
 import requests
+
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
 
 # Create your models here.
 class Person(models.Model):
@@ -50,6 +65,40 @@ class Person(models.Model):
         Person.surname=rand1+rand2
 
 
+
+    def generating_date(self):
+        rand=random.randint(0,3655)
+        if(rand<580):
+            #YEARS 1970-1976
+            d1 = datetime.strptime('1/1/1970 1:30 PM', '%m/%d/%Y %I:%M %p')
+            d2 = datetime.strptime('12/31/1975 4:50 AM', '%m/%d/%Y %I:%M %p')
+        if(rand<1280):
+            d1 = datetime.strptime('1/1/1976 1:30 PM', '%m/%d/%Y %I:%M %p')
+            d2 = datetime.strptime('12/31/1981 4:50 AM', '%m/%d/%Y %I:%M %p')
+            #YEARS 1976-1982
+        if(rand<1930):
+            d1 = datetime.strptime('1/1/1982 1:30 PM', '%m/%d/%Y %I:%M %p')
+            d2 = datetime.strptime('12/31/1987 4:50 AM', '%m/%d/%Y %I:%M %p')
+            #YEARS 1982-1988
+        if(rand<2480):
+            d1 = datetime.strptime('1/1/1988 1:30 PM', '%m/%d/%Y %I:%M %p')
+            d2 = datetime.strptime('12/31/1993 4:50 AM', '%m/%d/%Y %I:%M %p')
+            #YEARS 1988-1994
+        if(rand<2880):
+            d1 = datetime.strptime('1/1/2008 1:30 PM', '%m/%d/%Y %I:%M %p')
+            d2 = datetime.strptime('31/12/1999 4:50 AM', '%m/%d/%Y %I:%M %p')
+            #YEARS 1994-2000
+        if(rand<3255):
+            d1 = datetime.strptime('1/1/2000 1:30 PM', '%m/%d/%Y %I:%M %p')
+            d2 = datetime.strptime('12/31/2005 4:50 AM', '%m/%d/%Y %I:%M %p')
+            #YEARS 2000-2006
+        if(rand<3655):
+            d1 = datetime.strptime('1/1/2006 1:30 PM', '%m/%d/%Y %I:%M %p')
+            d2 = datetime.strptime('12/31/2013 4:50 AM', '%m/%d/%Y %I:%M %p')
+            #YEARS 2006-2014
+
+            
+        Person.birthday=random_date(d1,d2)
 
     def __str__(self):
         return self.pesel
